a0540cf2f94f84d0ef843b7e0190c3daa00ec960
Fabian Emmanuel <emmanuel.fabian.dev@gmail.com>
2022-03-13 13:57:20 +0100

    handle login view exception

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -6,6 +6,7 @@ import 'package:noteefy/exceptions/auth_exceptions.dart';
 import 'package:noteefy/services/auth/auth_service.dart';
 import 'package:noteefy/services/auth/bloc/auth_bloc.dart';
 import 'package:noteefy/services/auth/bloc/auth_event.dart';
+import 'package:noteefy/services/auth/bloc/auth_state.dart';
 import 'package:noteefy/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -57,24 +58,26 @@ class _LoginViewState extends State<LoginView> {
               hintText: 'Enter Your Password',
             ),
           ),
-          TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-                try {
-                  context.read<AuthBloc>().add(AuthEventLogIn(email, password));
-                } on UserNotFoundAuthException {
-                  await showErrorDialog(context, 'User Not Found!');
-                } on WrongPasswordAuthException {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is UserNotFoundAuthException) {
+                  await showErrorDialog(context, 'User not found!');
+                } else if (state.exception is WrongPasswordAuthException) {
                   await showErrorDialog(context, 'Wrong Credentials!');
-                } on InvalidEmailAuthException {
-                  await showErrorDialog(
-                      context, 'Invalid Email! Please Enter a valid Email');
-                } on GenericAuthException {
-                  await showErrorDialog(context, 'Authentication Error');
+                } else if (state.exception is GenericAuthException) {
+                  await showErrorDialog(context, 'Authentication error!');
                 }
-              },
-              child: const Text('Login')),
+              }
+            },
+            child: TextButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
+                  context.read<AuthBloc>().add(AuthEventLogIn(email, password));
+                },
+                child: const Text('Login')),
+          ),
           TextButton(
             onPressed: () {
               Navigator.of(context)
